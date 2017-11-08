df753b7bc744b9d91d0396ada6865a75ff0aa6be
Tra Nguyen <tnguye45@hawk.iit.edu>
2017-11-07 20:17:28 -0600

    Edit javascript for abbr to expand

--- js/site.js
+++ js/site.js
@@ -4,3 +4,10 @@ $('a[href^="http"]').on('click'), function(e) {
   window.open($(this).attr('href'), "_blank") /* refer as jquery object to file a new window event & open window in new tab, provide window default*/
   e.preventDefault();
 }); /* start with http, call out attribute names*/ /*create event on user's touch: click event & file event*/
+
+$('abbr[title]').one('click',function(){
+  var expanssion = $(this).attr('title');
+  var abbr = $(this).text(;
+  $(this).text(exapnsion + " (" +abbr+ ") ");
+  $(this).addClass('is-exapnded');
+})  /*to file up abbr when click in the website and also in html adjust the color, text decoration in abbr; with one-show the abbr only 1; but with on, it will go forever*/
